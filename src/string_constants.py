36a231ccd1f1b86accef380e91772765a5afcb92
kingusiu <kinga.wozniak@gmail.com>
2023-01-20 18:01:32 +0100

    adding notebooks folder for jupyter with playground subfolder for experimental scripts, updating result paths in string-constants, updating parameter setup in mockup gof

--- src/string_constants.py
+++ src/string_constants.py
@@ -5,7 +5,7 @@ import pathlib
 # -------------- #
 
 # root - in - data
-# root - out - histograms
+# root - out - data (histograms)
 # root - out - figures (histograms)
 
 # *********************************************************** #
@@ -14,18 +14,18 @@ import pathlib
 
 # root - in - data(polynomials cut outputs with 'sel' columns per quantile)
 # => directory structure: qr_run_x/env_run_Y/poly_run_Z
-root_in_data_basedir = '/eos/user/k/kiwoznia/data/QR_results/events'
+qr_results_in_data_basedir = '/eos/user/k/kiwoznia/data/QR_results/events'
 
-def get_root_in_data_dir(params):
-    data_dir = os.path.join(root_in_data_basedir,'qr_run_'+str(int(params.qr_run_n)),'env_run_'+str(int(params.env_run_n)),'poly_run_'+str(int(params.poly_run_n)))
+def get_qr_results_in_data_dir(params):
+    data_dir = os.path.join(qr_results_in_data_basedir,'qr_run_'+str(int(params.qr_run_n)),'env_run_'+str(int(params.env_run_n)),'poly_run_'+str(int(params.poly_run_n)))
     return data_dir
 
 # output directory for root histograms constructed from event selection values
 
-root_out_hist_basedir = 'results/hist'
+results_data_basedir = 'results/hist'
 # => directory structure: qr_run_x/env_run_Y/poly_run_Z
-def get_root_out_hist_dir(params):
-	data_dir = os.path.join(root_out_hist_basedir,'qr_run_'+str(int(params.qr_run_n)),'env_run_'+str(int(params.env_run_n)),'poly_run_'+str(int(params.poly_run_n)))
+def get_sel_histograms_out_data_dir(params):
+	data_dir = os.path.join(results_data_basedir, 'qr_run_'+str(int(params.qr_run_n)), 'env_run_'+str(int(params.env_run_n)), 'poly_run_'+str(int(params.poly_run_n)))
 	pathlib.Path(data_dir).mkdir(parents=True, exist_ok=True)
 	return data_dir
 
@@ -33,4 +33,17 @@ def get_root_out_hist_dir(params):
 #                           OUT FIGURES                       #
 # *********************************************************** #
 
+results_fig_basedir = 'results/fig'
 
+# => directory structure: qr_run_x/env_run_Y/poly_run_Z
+def get_sel_histograms_out_fig_dir(params):
+	results_selection_hists_subsir = 'quantile_sel_hists'
+	data_dir = os.path.join(results_fig_basedir,'qr_run_'+str(int(params.qr_run_n)),'env_run_'+str(int(params.env_run_n)),'poly_run_'+str(int(params.poly_run_n)), results_selection_hists_subsir)
+	pathlib.Path(data_dir).mkdir(parents=True, exist_ok=True)
+	return data_dir
+
+def get_stats_test_fig_dir(params):
+	results_stats_test_subdir = 'stats_tests'
+	data_dir = os.path.join(results_fig_basedir,'qr_run_'+str(int(params.qr_run_n)),'env_run_'+str(int(params.env_run_n)),'poly_run_'+str(int(params.poly_run_n)), results_stats_test_subdir)
+	pathlib.Path(data_dir).mkdir(parents=True, exist_ok=True)
+	return data_dir
